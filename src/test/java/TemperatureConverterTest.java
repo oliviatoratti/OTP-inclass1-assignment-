8c369965c9e0d4b71f0e58a172f606a61bdde652
Olivia Toratti <torattiolivia@gmail.com>
2026-09-06 22:25:33 +0300

    Completed JUnit TemperatureConverter Java assignment

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureConverterTest {
+    TemperatureConverter converter = new TemperatureConverter();
+
+    @Test
+    void fahrenheitToCelsiusTest() {
+        assertEquals(0, converter.fahrenheitToCelsius(32));
+        assertEquals(100, converter.fahrenheitToCelsius(212));
+    }
+    @Test
+    void celsiusToFahrenheitTest() {
+        assertEquals(32, converter.celsiusToFahrenheit(0));
+        assertEquals(212, converter.celsiusToFahrenheit(100));
+    }
+    @Test
+    void extremeTemperatureTest() {
+        assertTrue(converter.isExtremeTemperature(-41));
+        assertTrue(converter.isExtremeTemperature(51));
+
+        assertFalse(converter.isExtremeTemperature(-40));
+        assertFalse(converter.isExtremeTemperature(50));
+        assertFalse(converter.isExtremeTemperature(20));
+    }
+}
